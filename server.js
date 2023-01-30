06007e84821c117e392e571a4decc2c51267baca
PetkovBG <97334281+PetkovBG@users.noreply.github.com>
2023-01-30 11:09:01 +0200

    Setting up server on port 5000 and view engine - EJS

--- server.js
+++ server.js
@@ -2,5 +2,11 @@ const express = require('express');
 
 const app = express();
 
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+    res.send('Hello from EJS')
+});
+
 app.listen(5000);
 console.log('Server is running on port 5000.');
