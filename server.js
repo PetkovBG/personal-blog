73e5263173140a7876855f047ef3d3d65ae40aef
PetkovBG <97334281+PetkovBG@users.noreply.github.com>
2023-01-30 16:14:45 +0200

    Added sort view - showing latest articles first

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const articleRouter = require('./routes/articles');
 const mongoose = require('mongoose');
+const Article = require('./models/article');
 
 const app = express();
 
@@ -11,17 +12,10 @@ app.set('view engine', 'ejs');
 
 app.use(express.urlencoded({ extended: false }))
 
-app.get('/', (req, res) => {
-    const articles = [{
-        title: 'Test article',
-        createdAt: new Date(),
-        description: 'Test description'
-    },
-    {
-        title: 'Test article 2',
-        createdAt: new Date(),
-        description: 'Test description 2'
-    }]
+app.get('/', async (req, res) => {
+    const articles = await Article.find().sort({
+         createdAt: 'desc'
+    })
     res.render('articles/index', { articles: articles });
 });
 
