5fbf8968601cf367e7e6ea4cf529094f19e3a4bf
PetkovBG <97334281+PetkovBG@users.noreply.github.com>
2023-01-30 11:26:36 +0200

    Adding bootstrap link for styling

--- server.js
+++ server.js
@@ -8,7 +8,12 @@ app.set('view engine', 'ejs');
 app.use('/articles', articleRouter);
 
 app.get('/', (req, res) => {
-    res.render('index');
+    const articles = [{
+        title: 'Test article',
+        createdAt: Date.now(),
+        description: 'Test description'
+    }]
+    res.render('index', { articles: articles });
 });
 
 app.listen(5000);
