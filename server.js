85a4920b05f77172932317c65c503a47df28b091
PetkovBG <97334281+PetkovBG@users.noreply.github.com>
2023-01-30 11:15:41 +0200

    Creating routes folder for different routes

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 const express = require('express');
+const articleRouter = require('./routes/articles')
 
 const app = express();
 
 app.set('view engine', 'ejs');
 
+app.use('/articles', articleRouter);
+
 app.get('/', (req, res) => {
-    res.send('Hello from EJS')
+    res.render('index');
 });
 
 app.listen(5000);
