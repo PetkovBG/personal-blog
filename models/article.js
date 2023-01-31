2d737b3e1dbb2c7849edfb059c0e66813d86dfa6
PetkovBG <97334281+PetkovBG@users.noreply.github.com>
2023-01-31 11:22:38 +0200

    Converting markdown to HTML

--- models/article.js
+++ models/article.js
@@ -1,6 +1,9 @@
 const mongoose = require('mongoose');
 const marked = require('marked');
 const slugify = require('slugify');
+const createDomPurify = require('dompurify');
+const { JSDOM } = require('jsdom');
+const dompurify = createDomPurify(new JSDOM().window);
 
 const articleSchema = new mongoose.Schema({
     title: {
@@ -23,11 +26,19 @@ const articleSchema = new mongoose.Schema({
         required: true,
         unique: true
     },
+    sanitizedHTML: {
+        type: String,
+        required: true,
+    }
 });
 
 articleSchema.pre('validate', function(next) {
     if (this.title) {
         this.slug = slugify(this.title, { lower: true, strict: true })
+    }
+
+    if(this.markdown) {
+        this.sanitizedHTML = dompurify.sanitize(marked(this.markdown))
     }
 
     next();
