b8d280e8a79e3184120230dbced20e69b7939659
PetkovBG <97334281+PetkovBG@users.noreply.github.com>
2023-01-31 10:57:22 +0200

    Changing the path to be the slug instead of the id of the article for better user experience

--- routes/articles.js
+++ routes/articles.js
@@ -6,8 +6,8 @@ router.get('/new', (req, res) => {
     res.render('articles/new', { article: new Article() })
 });
 
-router.get('/:id', async (req, res) => {
-    const article = await Article.findById(req.params.id);
+router.get('/:slug', async (req, res) => {
+    const article = await Article.findOne( { slug: req.params.slug });
 
     if(article == null) {
         res.redirect('/')
@@ -23,11 +23,13 @@ router.post('/', async (req, res) => {
     });
     try {
         article = await article.save();
-        res.redirect(`/articles/${article.id}`)
+        res.redirect(`/articles/${article.slug}`)
 
     } catch(err) {
         res.render('articles/new', { article: article })
     }
 })
+
+router.delete
 
 module.exports = router;
