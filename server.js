00d07bc0827244237a6b4611685bd5d8251e1ca6
PetkovBG <97334281+PetkovBG@users.noreply.github.com>
2023-01-31 11:17:31 +0200

    Finished delete article option

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const articleRouter = require('./routes/articles');
 const mongoose = require('mongoose');
 const Article = require('./models/article');
+const methodOverride = require('method-override'); 
 
 const app = express();
 
@@ -11,6 +12,7 @@ mongoose.connect('mongodb://localhost/blog')
 app.set('view engine', 'ejs');
 
 app.use(express.urlencoded({ extended: false }))
+app.use(methodOverride('_method'));
 
 app.get('/', async (req, res) => {
     const articles = await Article.find().sort({
